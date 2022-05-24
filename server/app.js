48b97781e14beb353bf74fe886ed3acf6150e501
MarjuDox <nguyenson09082017@gmail.com>
2022-05-24 22:49:02 +0700

    Connect server

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const { Socket } = require('socket.io');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,{
+    allowEIO3: true,
+    cors:{
+        origin:"*",
+    },
+    transports:["polling","websocket"],
+});
 
 io.on("connection",(socket) =>{
     console.log("a user connected");
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(5000, () => {
+  console.log('listening on *:5000');
 });
