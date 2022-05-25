031408dabc35051e8b0fb971558052eda38730d7
MarjuDox <nguyenson09082017@gmail.com>
2022-05-25 12:47:39 +0700

    .

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -71,11 +71,7 @@ import { onMounted,ref } from '@vue/runtime-core';
         name:'Chatroom-view',
 		data() {
 			return {
-<<<<<<< HEAD
-				userName: "",
-=======
 				userName: '',
->>>>>>> 56f08074c6127ea41d5128cfd61cd7393240b9f3
 				joined: false,
 			}
 		},
@@ -100,6 +96,7 @@ import { onMounted,ref } from '@vue/runtime-core';
 		},
     
 		setup(){
+			const users = ref ([])
 			onMounted(() => {
 				socket.on("getUsers",(data) => {
 					data.forEach( user => {
